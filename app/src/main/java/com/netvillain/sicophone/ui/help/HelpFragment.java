225166935da2fdd4c487b284515fbfbac66b0555
zicchi <zidanezicchi@gmail.com>
2021-06-19 06:31:47 +0700

    Initial commit

---
+++ app/src/main/java/com/netvillain/sicophone/ui/help/HelpFragment.java
@@ -0,0 +1,25 @@
+package com.netvillain.sicophone.ui.help;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.netvillain.sicophone.R;
+
+public class HelpFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+    }
+}
